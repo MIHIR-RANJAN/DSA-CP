c024bd2be98ef903a9999da0e6819b71ba7c6086
DOCTOR <73585844+MIHIR-RANJAN@users.noreply.github.com>
2021-02-17 01:21:16 +0530

    Delete PYTH/Pattern directory

--- PYTH/Pattern/Pattern3.py
+++
@@ -1,9 +0,0 @@
-b=int(input("enter the no of rows: "))
-a=b+2
-for i in range(1,a+1):
-    for j in range(1,i):
-        print(" ",end=' ')
-    for j in range(1,a-i):
-        print("*",end=" ")
-    print()
-        
--- PYTH/Pattern/pattern1.py
+++
@@ -1,5 +0,0 @@
-a=int(input("inter the no of rows:"))
-for i in range(1,a+1):
-    for j in range(1,a+1):
-        print("* ", end=' ')
-    print()
