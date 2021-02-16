997bbf8b5a6cdf5292cc31dec92496f223097ce3
DOCTOR <73585844+MIHIR-RANJAN@users.noreply.github.com>
2021-02-17 01:21:27 +0530

    Delete PYTH/forLoop directory

--- PYTH/forLoop/EvenNumber.py
+++
@@ -1,3 +0,0 @@
-a=int(input("enter the limit: "))
-for i in range(2,a+1,2):
-    print(i, end=' ')
--- PYTH/forLoop/sumOfnNo.py
+++
@@ -1,5 +0,0 @@
-a=int(input("enter the limit: "))
-sum=0
-for i in range(1,a+1):
-    sum=sum+i
-print("sum of the no is:",sum)
