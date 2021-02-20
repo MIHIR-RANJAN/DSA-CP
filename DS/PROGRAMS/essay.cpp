319cc270b6dc866c375c0b82699e5ec1aa993694
Mihir Ranjan <menkadevi1975@gmail.com>
2021-02-20 14:37:53 +0530

    add

---
+++ DS/PROGRAMS/essay.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int n;
+    int k;
+    cout<<"enter the no of words: ";
+    cin>> n;
+    cout<<"enter the no of one line: ";
+    cin>> k;
+    string arr[n];
+    cout<<"enter the sentence: ";
+    for(int i=0;i<n; i++){
+        
+        cin>> arr[i];
+        int a = arr[i].length();
+        
+        
+        
+
+    }
+    
+    for (int i =0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<< a ;
+    
+    
+}
