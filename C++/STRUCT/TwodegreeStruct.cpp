5167757e48a3bf8bfe63bbb9344020b217279329
Mihir Ranjan <menkadevi1975@gmail.com>
2021-04-20 20:45:22 +0530

    2D struct

---
+++ C++/STRUCT/TwodegreeStruct.cpp
@@ -0,0 +1,78 @@
+// talk about phone of ppl of ur house 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct phone{
+    int oneplus;
+    int vivo;
+    int pocoS;
+    int pocoB;
+}po;
+struct house{
+    phone dad;
+    phone mom;
+    phone bro;
+    phone my;
+}ho;
+int main(){
+
+    struct house ram , storage;
+    cout<<"\n";
+    cout<<"\n";
+    cout<<"its ram time bitch";
+    cout<<"\n";
+    cout<<"\n";
+    cout<<"enter the ram of dad's phone :";
+    cin>>ram.dad.oneplus;
+    cout<<"enter the ram of mom's phone :";
+    cin>>ram.mom.vivo;
+    cout<<"enter the ram of bro's phone :";
+    cin>>ram.bro.pocoS;
+    cout<<"enter the ram of my phone :";
+    cin>>ram.my.pocoB;
+    cout<<"\n";
+    cout<<"\n";
+    cout<<"|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||";
+    cout<<"\n";
+    cout<<"\n";
+    cout<<"its storage time bitch";
+    cout<<"\n";
+    cout<<"\n";
+    cout<<"enter the storage in gb of dad's phone :";
+    cin>>storage.dad.oneplus;
+    cout<<"enter the storage in gb of mom's phone :";
+    cin>>storage.mom.vivo;
+    cout<<"enter the storage in gb of bro's phone :";
+    cin>>storage.bro.pocoS;
+    cout<<"enter the storage in gb of my phone :";
+    cin>>storage.my.pocoB;
+    cout<<"\n";
+    cout<<"\n";
+    cout<<"|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||";
+    cout<<"\n";cout<<"\n";
+    cout<<"its printing time bitch";
+    cout<<"\n";cout<<"\n"; 
+    cout<<"|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||";
+    cout<<"\n";cout<<"\n";    
+    cout<<"first lets go with ram";
+    cout<<"\n";cout<<"\n";
+    cout<<"the ram of  dad's phone is "<<ram.dad.oneplus<<endl;
+    cout<<"the ram of  mom's phone is "<<ram.mom.vivo<<endl;
+    cout<<"the ram of  bros phone is "<<ram.bro.pocoS<<endl;
+    cout<<"the ram of  my phone is "<<ram.my.pocoB<<endl;
+    cout<<"|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||";
+    cout<<"\n";cout<<"\n";    
+    cout<<"lets go with storage now";
+    cout<<"\n";cout<<"\n";
+    cout<<"storage of dad's phone is "<<storage.dad.oneplus<<endl;
+    cout<<"storage of mom's phone is "<<storage.mom.vivo<<endl;
+    cout<<"storage of bros phone is "<<storage.bro.pocoS<<endl;
+    cout<<"storage of my phone is "<<storage.my.pocoB<<endl;
+
+
+
+
+
+
+}
---
+++ C++/STRUCT/onedegreeMeTwo.cpp
@@ -0,0 +1,69 @@
+// my room things - and my brother things
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct myHouse{
+    int noBed;
+    int noGym;
+    int noBooks;
+    int noWaste;
+
+};
+
+int main(){
+
+    struct myHouse myRoom,broRoom;
+    cout<<"the no of bed is : ";
+    cin>>myRoom.noBed;
+    cout<<"n";
+    cout<<"the no of gym material is : ";
+    cin>>myRoom.noGym;
+    cout<<"n";
+    cout<<"the no of books is : ";
+    cin>>myRoom.noBooks;
+    cout<<"n";
+    cout<<"the no of waste incuding me is : ";
+    cin>>myRoom.noWaste;
+    cout<<"\n";
+    cout<<"\n";
+    cout<<"its your brother time bitch";
+    cout<<"\n";
+    cout<<"\n";
+
+
+    cout<<"the no of bed bro have : ";
+    cin>>broRoom.noBed;
+    cout<<"n";
+    cout<<"the no of gym material he have : ";
+    cin>>broRoom.noGym;
+    cout<<"n";
+    cout<<"the no of books he read : ";
+    cin>>broRoom.noBooks;
+    cout<<"n";
+    cout<<"the no of waste incuding him is : ";
+    cin>>broRoom.noWaste;
+
+    cout<<"\n";
+    cout<<"\n";
+    cout<<"|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||";
+    cout<<"\n";
+    cout<<"\n";
+    
+    cout<<"the no of bed is "<< myRoom.noBed<<endl;
+    cout<<"the no of gym material is "<< myRoom.noGym<<endl;
+    cout<<"the no of books is "<< myRoom.noBooks<<endl;
+    cout<<"the no of waste incuding me is "<< myRoom.noWaste<<endl;
+    cout<<"\n";
+    cout<<"\n";
+    cout<<"its your brother time bitch";
+    cout<<"\n";
+    cout<<"\n";
+    cout<<"the no of bed he have "<< broRoom.noBed<<endl;
+    cout<<"the no of gym material he have  "<< broRoom.noGym<<endl;
+    cout<<"the no of books he read "<< broRoom.noBooks<<endl;
+    cout<<"the no of waste incuding him is "<< broRoom.noWaste<<endl;
+
+
+
+}
